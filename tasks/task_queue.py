98d31b105a98ddc8876d3df2fe432dfbfd852eb3
chun <peterpeter200412@gmail.com>
2025-04-26 23:10:24 +0800

    Add input parameter of load_tasks

--- tasks/task_queue.py
+++ tasks/task_queue.py
@@ -1,7 +1,7 @@
 import json
 
-def load_tasks():
+def load_tasks(file_path):
     # file_path = "task/tasks_taipei_100.json"
-    file_path = "./tasks/sample/tasks_taoyuan_100.json"
+    # file_path = "./tasks/sample/tasks_taoyuan_100.json"
     with open(file_path) as f:
         return json.load(f)
